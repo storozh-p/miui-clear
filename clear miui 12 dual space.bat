cef5ce1ffc22c5490d751e562ddf3315713cb654
Petya <135897547+storozh-p@users.noreply.github.com>
2024-01-18 13:23:00 +0200

    Add files via upload

---
+++ clear miui 12 dual space.bat
@@ -0,0 +1,65 @@
+adb shell pm uninstall --user 10 com.miui.fm
+adb shell pm uninstall --user 10 com.miui.compass
+adb shell pm uninstall --user 10 com.miui.analytics
+adb shell pm uninstall --user 10 com.xiaomi.mipicks
+adb shell pm uninstall --user 10 com.miui.msa.global
+adb shell pm uninstall --user 10 com.miui.cloudservice
+adb shell pm uninstall --user 10 com.miui.cloudservice.sysbase
+adb shell pm uninstall --user 10 com.miui.newmidrive
+adb shell pm uninstall --user 10 com.miui.cloudbackup
+adb shell pm uninstall --user 10 com.miui.backup
+adb shell pm uninstall --user 10 com.xiaomi.glgm
+adb shell pm uninstall --user 10 com.xiaomi.payment
+adb shell pm uninstall --user 10 com.mipay.wallet.in
+adb shell pm uninstall --user 10 com.tencent.soter.soterserver
+adb shell pm uninstall --user 10 cn.wps.xiaomi.abroad.lite
+adb shell pm uninstall --user 10 com.miui.videoplayer
+adb shell pm uninstall --user 10 com.miui.player
+adb shell pm uninstall --user 10 com.mi.globalbrowser
+adb shell pm uninstall --user 10 com.xiaomi.midrop
+adb shell pm uninstall --user 10 com.miui.yellowpage
+adb shell pm uninstall --user 10 com.miui.android.fashiongallery
+adb shell pm uninstall --user 10 com.miui.bugreport
+adb shell pm uninstall --user 10 com.miui.miservice
+adb shell pm uninstall --user 10 com.miui.weather2
+adb shell pm uninstall --user 10 com.miui.hybrid
+adb shell pm uninstall --user 10 com.miui.hybrid.accessory
+adb shell pm uninstall --user 10 com.miui.global.packageinstaller
+adb shell pm uninstall --user 10 com.xiaomi.joyose
+
+adb shell pm uninstall --user 10 com.google.android.gms.location.history
+adb shell pm uninstall --user 10 com.google.android.videos
+adb shell pm uninstall --user 10 com.google.android.music
+adb shell pm uninstall --user 10 com.google.android.apps.photos
+adb shell pm uninstall --user 10 com.google.android.apps.tachyon
+adb shell pm uninstall --user 10 com.google.ar.lens
+adb shell pm uninstall --user 10 com.google.android.googlequicksearchbox
+adb shell pm uninstall --user 10 com.google.android.apps.wellbein
+adb shell pm uninstall --user 10 com.google.android.apps.googleassistant
+
+adb shell pm uninstall --user 10 com.facebook.katana
+adb shell pm uninstall --user 10 com.facebook.services
+adb shell pm uninstall --user 10 com.facebook.system
+adb shell pm uninstall --user 10 com.facebook.appmanager
+
+adb shell pm uninstall --user 10 com.android.browser
+adb shell pm uninstall --user 10 com.android.wallpaper.livepicker
+adb shell pm uninstall --user 10 com.android.dreams.basic
+adb shell pm uninstall --user 10 com.android.dreams.phototable
+
+adb shell pm uninstall --user 10 com.netflix.partner.activation
+adb shell pm uninstall --user 10 com.zhilioapp.musically
+adb shell pm uninstall --user 10 ru.yandex.searchplugin
+adb shell pm uninstall --user 10 com.yandex.zen
+adb shell pm uninstall --user 10 com.ebay.mobile
+adb shell pm uninstall --user 10 com.ebay.carrier
+adb shell pm uninstall --user 10 ru.ozon.app.android
+adb shell pm uninstall --user 10 com.alibaba.aliexpresshd
+adb shell pm uninstall --user 10 sg.bigo.live
+adb shell pm uninstall --user 10 ru.auto.ara
+
+adb shell pm uninstall --user 10 com.android.stk
+adb shell pm uninstall --user 10 com.android.stk2
+
+echo "Dual space cleared!"
+pause
---
+++ clear miui 12.bat
@@ -0,0 +1,65 @@
+adb shell pm uninstall --user 0 com.miui.fm
+adb shell pm uninstall --user 0 com.miui.compass
+adb shell pm uninstall --user 0 com.miui.analytics
+adb shell pm uninstall --user 0 com.xiaomi.mipicks
+adb shell pm uninstall --user 0 com.miui.msa.global
+adb shell pm uninstall --user 0 com.miui.cloudservice
+adb shell pm uninstall --user 0 com.miui.cloudservice.sysbase
+adb shell pm uninstall --user 0 com.miui.newmidrive
+adb shell pm uninstall --user 0 com.miui.cloudbackup
+adb shell pm uninstall --user 0 com.miui.backup
+adb shell pm uninstall --user 0 com.xiaomi.glgm
+adb shell pm uninstall --user 0 com.xiaomi.payment
+adb shell pm uninstall --user 0 com.mipay.wallet.in
+adb shell pm uninstall --user 0 com.tencent.soter.soterserver
+adb shell pm uninstall --user 0 cn.wps.xiaomi.abroad.lite
+adb shell pm uninstall --user 0 com.miui.videoplayer
+adb shell pm uninstall --user 0 com.miui.player
+adb shell pm uninstall --user 0 com.mi.globalbrowser
+adb shell pm uninstall --user 0 com.xiaomi.midrop
+adb shell pm uninstall --user 0 com.miui.yellowpage
+adb shell pm uninstall --user 0 com.miui.android.fashiongallery
+adb shell pm uninstall --user 0 com.miui.bugreport
+adb shell pm uninstall --user 0 com.miui.miservice
+adb shell pm uninstall --user 0 com.miui.weather2
+adb shell pm uninstall --user 0 com.miui.hybrid
+adb shell pm uninstall --user 0 com.miui.hybrid.accessory
+adb shell pm uninstall --user 0 com.miui.global.packageinstaller
+adb shell pm uninstall --user 0 com.xiaomi.joyose
+
+adb shell pm uninstall --user 0 com.google.android.gms.location.history
+adb shell pm uninstall --user 0 com.google.android.videos
+adb shell pm uninstall --user 0 com.google.android.music
+adb shell pm uninstall --user 0 com.google.android.apps.photos
+adb shell pm uninstall --user 0 com.google.android.apps.tachyon
+adb shell pm uninstall --user 0 com.google.ar.lens
+adb shell pm uninstall --user 0 com.google.android.googlequicksearchbox
+adb shell pm uninstall --user 0 com.google.android.apps.wellbein
+adb shell pm uninstall --user 0 com.google.android.apps.googleassistant
+
+adb shell pm uninstall --user 0 com.facebook.katana
+adb shell pm uninstall --user 0 com.facebook.services
+adb shell pm uninstall --user 0 com.facebook.system
+adb shell pm uninstall --user 0 com.facebook.appmanager
+
+adb shell pm uninstall --user 0 com.android.browser
+adb shell pm uninstall --user 0 com.android.wallpaper.livepicker
+adb shell pm uninstall --user 0 com.android.dreams.basic
+adb shell pm uninstall --user 0 com.android.dreams.phototable
+
+adb shell pm uninstall --user 0 com.netflix.partner.activation
+adb shell pm uninstall --user 0 com.zhilioapp.musically
+adb shell pm uninstall --user 0 ru.yandex.searchplugin
+adb shell pm uninstall --user 0 com.yandex.zen
+adb shell pm uninstall --user 0 com.ebay.mobile
+adb shell pm uninstall --user 0 com.ebay.carrier
+adb shell pm uninstall --user 0 ru.ozon.app.android
+adb shell pm uninstall --user 0 com.alibaba.aliexpresshd
+adb shell pm uninstall --user 0 sg.bigo.live
+adb shell pm uninstall --user 0 ru.auto.ara
+
+adb shell pm uninstall --user 0 com.android.stk
+adb shell pm uninstall --user 0 com.android.stk2
+
+echo "Cleared!"
+pause
